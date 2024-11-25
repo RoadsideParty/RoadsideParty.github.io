463ce36e9a0375ecb13eb86131bebb3934190ead
psp <976477689@qq.com>
2024-11-25 13:47:44 +0800

    update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,16 @@
+import { h } from "vue"
 import type { Theme } from "vitepress"
 import DefaultTheme from "vitepress/theme"
 import Image from "./components/Image.vue"
+import NotFound from "./components/NotFound.vue"
 
 export default {
 	extends: DefaultTheme,
+	Layout() {
+		return h(DefaultTheme.Layout, null, {
+			"not-found": () => h(NotFound),
+		})
+	},
 	enhanceApp({ app }) {
 		app.component("Image", Image)
 	},
